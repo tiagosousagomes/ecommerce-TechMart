82d3020f0e37630ba36bb6a1ee5e8ee2d52e4372
ocabecinha <mateuspatricio1@hotmail.com>
2024-06-05 20:12:55 -0300

    Tratamento de Erros
    Tratamento dos erros da área de registro, minha próxima atualização vai ser focada em melhorar a Home.

--- TechtMart/login/register.js
+++ TechtMart/login/register.js
@@ -7,6 +7,17 @@ function register(){
     firebase.auth().createUserWithEmailAndPassword(email,password).then(()=>{
         alert('email registrado com sucesso"')
         window.location.href = '../home/home.html'}).catch(error =>{
-            alert('email já registrado')
+            alert(handleAuthError(error));
         })
+    }
+    function handleAuthError(error) {
+
+        if(error.code == "auth/weak-password"){
+            return "Senha deve ter no minimo 6 caracteres."
+        }else if(error.code == "auth/invalid-email"){
+            return "E-mail inválido."
+        }else if(error.code == "auth/email-already-in-use"){
+            return "E-mail já cadastrado."
+        }
+        return error.message;
     }

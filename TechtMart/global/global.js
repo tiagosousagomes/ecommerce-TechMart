0de4a4c4205df855cd928973be39f327759b4429
Tiago Sousa <tiagosousagomes14@gmail.com>
2024-06-15 18:31:24 -0300

    criada a pagina about

--- TechtMart/global/global.js
+++ TechtMart/global/global.js
@@ -1,15 +1,18 @@
 //cria um menu suspenso ao clicar na imagem de perfil no nav
 
-document.getElementById('imgPerfil').addEventListener('click', function() {
-    var dropdownMenu = document.getElementById('dropdownMenu');
-    dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
+document.getElementById('imgPerfil').addEventListener('click', function()
+{
+	var dropdownMenu = document.getElementById('dropdownMenu');
+	dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
 });
-
-window.onclick = function(event) {
-    if (!event.target.matches('.img-perfil')) {
-        var dropdownMenu = document.getElementById('dropdownMenu');
-        if (dropdownMenu.style.display === 'block') {
-            dropdownMenu.style.display = 'none';
-        }
-    }
+window.onclick = function(event)
+{
+	if (!event.target.matches('.img-perfil'))
+	{
+		var dropdownMenu = document.getElementById('dropdownMenu');
+		if (dropdownMenu.style.display === 'block')
+		{
+			dropdownMenu.style.display = 'none';
+		}
+	}
 };

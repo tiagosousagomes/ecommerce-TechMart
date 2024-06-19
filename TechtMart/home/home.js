1e5a50cdbcbb6270cf305cbc9adf384aaad1dfc6
Felipe <felipeferreira3146@gmail.com>
2024-06-19 09:55:23 -0300

    Adicionando icone de favorito ao primeiro produto

--- TechtMart/home/home.js
+++ TechtMart/home/home.js
@@ -8,3 +8,29 @@ function logout(){
         alert('erro ao fazer logout')
     })   
 }
+
+/*function alternarCoracao() {
+    var heartIcon = document.getElementById('heart-icon');
+    if (heartIcon == 'fa-sharp fa-regular fa-heart fa-2x') {
+        heartIcon == 'fa-sharp fa-solid fa-herat fa-2x';
+        //heartIcon.classList.add('fa-solid');
+    } else {
+        heartIcon.classList.remove('fa-solid');
+        heartIcon.classList.add('fa-regular');
+    }
+}*/
+
+
+/*document.addEventListener('DOMContentLoaded', () => {
+      document.addEventListener('click', (event) => {
+        if (event.target.classList.contains('heart-icon')) {
+            toggleIcon(event);
+        }
+    });
+});*/
+
+function toggleIcon() {
+    var icon = document.getElementById('hearth-icon');
+    icon.classList.toggle('fa-regular');
+    icon.classList.toggle('fa-solid');
+}
